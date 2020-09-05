5ee2c289e8e8d1a27177f7428e68ba2c1f58be34
LuckEx123 <68332212+LuckEx123@users.noreply.github.com>
2020-09-05 10:58:32 +0700

    Fix lỗi Edit, Delete

--- BTL/Properties/Settings.Designer.cs
+++ BTL/Properties/Settings.Designer.cs
@@ -26,8 +26,8 @@ namespace BTL.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\LT\\C#\\BaiTapLon\\BaiTapLon\\" +
-            "DBQLBanHang.mdf;Integrated Security=True;Connect Timeout=30")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\LT\\BTL\\BTL\\DBQLBanHang.mdf" +
+            ";Integrated Security=True;Connect Timeout=30")]
         public string conStr {
             get {
                 return ((string)(this["conStr"]));
